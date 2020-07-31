262dd8d281a043f366e6f86d1e6c754766e7a333
Gustavo Xavier <guh.coostta@gmail.com>
2020-07-30 23:00:34 -0300

    Clicando mais de uma vez no botão igual

--- scripts/controler/CalcController.js
+++ scripts/controler/CalcController.js
@@ -31,6 +31,9 @@ class CalcController {
    
     constructor(){
         
+        this._lastOperator = ''
+        this._lastNumber = ''
+
         this._operation = []
         this._locale = 'pt-BR'
         this._displayCalcEl = document.querySelector("#display") 
@@ -108,17 +111,42 @@ class CalcController {
 
     }
 
+    getResult() {
+
+        console.log("getResult", this._operation)
+
+        return eval(this._operation.join(""))
+    }
+
     calc() {
 
         let last = ''
-        if(this._operation.length > 3) {
-            last = this._operation.pop()
+
+        this._lastOperator = this.getLastItem()
+
+        if(this._operation.length < 3) {
+
+            let firstItem = this._operation[0]
+            this._operation = [firstItem, this._lastOperator, this._lastNumber]
 
         }
-        
-        let result = eval(this._operation.join(""))
 
-        if(last == '%') {
+        if (this._operation.length > 3) {
+
+            last = this._operation.pop()
+            
+            this._lastNumber = this.getResult()
+        } else if (this._operation.length == 3) {
+
+                this._lastNumber = this.getLastItem(false)
+
+            }
+        
+
+        
+        let result = this.getResult()
+
+        if (last == '%') {
 
             result /= 100
 
@@ -137,19 +165,37 @@ class CalcController {
         this.setLastNumberToDisplay()
     }
 
+    getLastItem(isOperator = true) {
+
+        let lastItem
+
+        for(let i = this._operation.length-1; i >= 0; i--) {
+
+            if(this.isOperator(this._operation[i]) == isOperator){
+                lastItem = this._operation[i]
+                break
+           }
+
+            
+        }
+
+        if(!lastItem) {
+
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber
+
+        }
+
+        return lastItem
+    }
+    
+
     // Mostra o resultado do calculo entre os números pares da calculadora
     setLastNumberToDisplay() {
 
-        let lastNumber
-        for(let i = this._operation.length-1; i >= 0; i--) {
-
-            if(!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i]
-                break
-            }
-        }
-
+        let lastNumber = this.getLastItem(false)
+        
         if (!lastNumber) lastNumber = 0
+
         this.displayCalc = lastNumber
     }
     
